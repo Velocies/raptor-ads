e21ac37bd4e7568e885b8be8c57d51eacf4d6d2d
Jimmie Gisclair <Jimmie Gisclair>
2017-02-17 10:50:40 -0800

    Modify routes.js to take two additional arguments

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const db = require('../database/schemas.js');
-require('./routes.js')(app, db);
 
 const port = process.env.PORT || 3000;
 
@@ -10,6 +9,7 @@ const rootPath = path.join(__dirname, '/..');
 
 app.use(express.static(rootPath));
 
+require('./routes.js')(app, db, path, rootPath);
 
 app.listen(port, () => {
   console.log('Example app listening on port', port);

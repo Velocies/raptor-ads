6667241d09d78438923c9ffbd4e8902759b1e532
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-17 17:50:04 -0800

    Add Signup form

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -7,14 +7,13 @@ export const Navbar = ({location}) => {
     <Segment inverted>
       <Menu inverted pointing secondary>
         <Menu.Item header>Our Company</Menu.Item>
-        <Menu.Item as={Link} to='landing' name='About' active={location === 'landing'} />
+        <Menu.Item as={Link} to='/landing' name='About' active={location === 'landing'} />
         <Menu.Item as={Link} to='/customer/dashboard' name='customer dashboard' active={location === '/customer/dashboard'} />
-
-          <Menu.Menu position='right'>
-            <Link to='signup' className='item'>
-              Sign Up
-            </Link>
-          <Menu.Item as={Link} to='login' name='login'>
+        <Menu.Menu position='right'>
+          <Link to='/signup' className='item'>
+            Sign Up
+          </Link>
+          <Menu.Item as={Link} to='/login' name='login'>
             Login
           </Menu.Item>
         </Menu.Menu>
--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,22 +1,46 @@
-import React from 'react';
-import { Button, Checkbox, Form, Container } from 'semantic-ui-react'
+import React, { Component } from 'react';
+import { Button, Checkbox, Form, Container, Grid, Header } from 'semantic-ui-react'
 
-export const Signup = () =>
-  <Container textAlign='center'>
-    <Form>
-      <Form.Group >
-        <Form.Field >
-          <label>First Name</label>
-          <input placeholder='First Name' />
-        </Form.Field>
-        <Form.Field>
-          <label>Last Name</label>
-          <input placeholder='Last Name' />
-        </Form.Field>
-      </Form.Group>
-        <Form.Field>
-          <Checkbox label='I agree to the Terms and Conditions' />
-        </Form.Field>
-        <Button type='submit'>Submit</Button>
-    </Form>
-  </Container>
+export default class Signup extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid textAlign="center" width={14}>
+          <Grid.Column width={4}>
+          </Grid.Column>
+          <Grid.Column width={9}>
+            <Form>
+              <Form.Field width="8">
+                <label>First Name</label>
+                <input placeholder='First Name' />
+              </Form.Field>
+              <Form.Field width="8">
+                <label>Last Name</label>
+                <input placeholder='Last Name' />
+              </Form.Field>
+              <Form.Field width="8">
+                <label>Email</label>
+                <input placeholder='Email' />
+              </Form.Field>
+              <Form.Field width="8">
+                <label>Password</label>
+                <input placeholder='Password' />
+              </Form.Field>
+              <Form.Field width="8">
+                <label>Confirm Password</label>
+                <input placeholder='Confirm Password' />
+              </Form.Field>
+              <Form.Field width="8">
+                <Button type='submit'>Sign Up</Button>
+              </Form.Field>
+            </Form>
+          </Grid.Column>
+        </Grid>
+      </div>
+    )
+  }
+}

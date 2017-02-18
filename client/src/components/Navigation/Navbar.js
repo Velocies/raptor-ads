17b328350c2ae67bbc1dcf4df7834142dfe713ab
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-17 19:52:50 -0800

    Fix logo

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -6,8 +6,8 @@ export const Navbar = ({location}) => {
   return (
     <Segment inverted>
       <Menu inverted pointing secondary>
-        <Menu.Item header><img src='/client/src/assets/half-raptor.png'/></Menu.Item>
-        <Menu.Item as={Link} to='/landing' name='About' active={location === 'landing'} />
+        <Menu.Item className="logo"><img src='/client/src/assets/half-raptor.png'/></Menu.Item>
+        <Menu.Item as={Link} to='/landing' name='About' active={location === '/landing'} />
         <Menu.Item as={Link} to='/customer/dashboard' name='customer dashboard' active={location === '/customer/dashboard'} />
         <Menu.Menu position='right'>
           <Link to='/signup' className='item'>

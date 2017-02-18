62fdaa2d821dc7d5c5dcbb115020cb6c5906ae06
bobby <bwuphan@gmail.com>
2017-02-17 19:45:29 -0800

    add raptor logo in navbar

--- client/src/components/Navigation/Navbar.js
+++ client/src/components/Navigation/Navbar.js
@@ -6,7 +6,7 @@ export const Navbar = ({location}) => {
   return (
     <Segment inverted>
       <Menu inverted pointing secondary>
-        <Menu.Item header>Our Company</Menu.Item>
+        <Menu.Item header><img src='/client/src/assets/half-raptor.png'/></Menu.Item>
         <Menu.Item as={Link} to='/landing' name='About' active={location === 'landing'} />
         <Menu.Item as={Link} to='/customer/dashboard' name='customer dashboard' active={location === '/customer/dashboard'} />
         <Menu.Menu position='right'>

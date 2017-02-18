cb665ea9ba1ec324cd0a306dfa95c00fc9ff2300
Darkrender <jimmiegisclair@gmail.com>
2017-02-17 19:59:31 -0800

    Implement getAll for userController. Close #44

--- server/config/users/userController.js
+++ server/config/users/userController.js
@@ -1,10 +1,15 @@
+const db = require('../../database/schemas.js');
+
 module.exports = {
   getOne: (req, res) => {
 
   },
 
   getAll: (req, res) => {
-
+    db.User.findAll({})
+    .then((users) => {
+      res.send(JSON.stringify(users));
+    });
   },
 
   createOne: (req, res) => {

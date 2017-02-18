71789c9e452cf850326684fa9c084093570f83a0
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-18 10:13:46 -0800

    Refactor to use object rest spread

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -7,7 +7,7 @@ export const initialState = {
 export const auth = (state=initialState, action) => {
   switch (action.type) {
     case TOGGLE_SIGNUP_FORM:
-      return Object.assign({}, state, {signupForm: {activeLink: action.link}});
+      return ({...state, signupForm: {activeLink: action.link}})
     default:
       return state;
   }

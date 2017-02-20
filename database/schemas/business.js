59a0d32fe5889d1070cb97c92cbd17d41c27a355
Darkrender <jimmiegisclair@gmail.com>
2017-02-20 10:53:41 -0800

    Fix styleguide issues in business.js

--- database/schemas/business.js
+++ database/schemas/business.js
@@ -1,14 +1,14 @@
-module.exports = (db, Sequelize, User) => {
-  var Business = db.define('business', {
+module.exports = (db, Sequelize) => {
+  const Business = db.define('business', {
     id: {
       type: Sequelize.INTEGER,
       autoIncrement: true,
-      primaryKey: true
+      primaryKey: true,
     },
     name: Sequelize.STRING,
     address: Sequelize.STRING,
     license: Sequelize.STRING,
-    user_id: Sequelize.INTEGER
+    user_id: Sequelize.INTEGER,
   });
 
   return Business;

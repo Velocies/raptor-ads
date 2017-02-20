784a960524bbbbf79d5734c0a0da48ab6bf0fd95
bobby <bwuphan@gmail.com>
2017-02-20 15:43:39 -0800

    add fetch data from customer submit

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,14 +1,29 @@
-import { TOGGLE_SIGNUP_FORM, CHANGE_SIGNUP_FIELD } from '../constants.js'
+import { TOGGLE_SIGNUP_FORM, CHANGE_SIGNUP_FIELD } from '../constants';
 
-export const toggleSignupLink = (link) =>
+export const toggleSignupLink = link =>
   ({
     type: TOGGLE_SIGNUP_FORM,
-    link
-  })
+    link,
+  });
 
 export const changeSignupField = (field, value) =>
   ({
     type: CHANGE_SIGNUP_FIELD,
     field,
-    value
-  })
+    value,
+  });
+
+export const customerSignup = customer =>
+  (dispatch) => {
+    console.log('customer is', customer);
+    fetch('/api/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(customer),
+    })
+    .then((user) => {
+      console.log('post to users returns', user);
+    });
+  };
--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux';
 import createLogger from 'redux-logger';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
 const configureStore = () => {
   const logger = createLogger();
-  return createStore(rootReducer, applyMiddleware(logger));
+  return createStore(rootReducer, applyMiddleware(logger, thunk));
 };
 
 export default configureStore;

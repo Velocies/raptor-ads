99c7d57b57599be265215601edd427f6f2b0bd30
Darkrender <jimmiegisclair@gmail.com>
2017-02-20 12:56:54 -0800

    Add tests for deleting a user from the database

--- server/tests/userController.test.js
+++ server/tests/userController.test.js
@@ -36,6 +36,7 @@ describe('Begin', () => {
         zip: '70542',
         country: 'Murica',
       };
+
       request(app)
         .post('/api/users')
         .send(data)
@@ -44,5 +45,94 @@ describe('Begin', () => {
           done();
         });
     });
+  });
+
+  describe('User Deletion', () => {
+    it('should delete a user from the database', (done) => {
+      const data = {
+        name: 'Cory Wolnewitz',
+        email: 'cwol@bigusdickus.com',
+        address: '1337 Cocksure Ln',
+        city: 'Jupiter',
+        state: 'FL',
+        zip: '70542',
+        country: 'Murica',
+      };
+
+      request(app)
+        .post('/api/users')
+        .send(data)
+        .end((postErr, postRes) => {
+          request(app)
+            .delete(`/api/users/${postRes.body.id}`)
+            .end(() => {
+              db.User.findOne({ where: { email: data.email } })
+              .then((user) => {
+                expect(user).to.equal(null);
+                done();
+              });
+            });
+        });
+    });
+
+    it('should delete the correct user from the database', (done) => {
+      const data = {
+        name: 'Cory Wolnewitz',
+        email: 'cwol@bigusdickus.com',
+        address: '1337 Cocksure Ln',
+        city: 'Jupiter',
+        state: 'FL',
+        zip: '70542',
+        country: 'Murica',
+      };
+
+      const data2 = {
+        name: 'Jimmie Gisclair',
+        email: 'jimmehsemail@herpderp.yolo',
+        address: '2257 meow meow lane',
+        city: 'Shoop',
+        state: 'LA',
+        zip: '70029',
+        country: 'Murica',
+      };
+      let existingUser;
+      let deletedUser;
+
+      request(app)
+        .post('/api/users')
+        .send(data)
+        .end(() => {
+          request(app)
+          .post('/api/users')
+          .send(data2)
+          .end((err, res) => {
+            request(app)
+              .delete(`/api/users/${res.body.id}`)
+              .end(() => {
+                db.User.findOne({ where: { id: res.body.id } })
+                  .then((user) => {
+                    deletedUser = user;
+
+                    db.User.findOne({ where: { email: data.email } })
+                      .then((result) => {
+                        existingUser = result;
+
+                        expect(existingUser).to.not.equal(null);
+                        expect(deletedUser).to.equal(null);
+                        done();
+                      });
+                  });
+              });
+          });
+        });
+    });
+  });
+
+  describe('User Patching', () => {
+
+  });
+
+  describe('User Retrieval', () => {
+
   });
 });

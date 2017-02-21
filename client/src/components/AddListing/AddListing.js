8e999a1814ed44829ba2851dc4db5bf20672f7b8
bobby <bwuphan@gmail.com>
2017-02-21 12:26:46 -0800

    add placeholder 'Add Listing'

---
+++ client/src/components/AddListing/AddListing.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default class AddListing extends Component {
+  render() {
+    return (
+      <h2>Add Listing</h2>
+    );
+  }
+}

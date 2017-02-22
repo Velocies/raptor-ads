f5151d8e03c9dc369d5e320366ec407efb0b94b3
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-21 16:37:41 -0800

    Add test for Signup Form

---
+++ client/src/components/Signup/tests/CustomerSignup.test.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-unused-vars */
+
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import CustomerSignup from '../CustomerSignup';
+import configureStore from '../../../configureStore';
+
+const store = configureStore();
+
+describe('CustomerSignup validations', () => {
+  const customerSignup = mount(
+    <Provider store={store}>
+      <CustomerSignup dispatch={store.dispatch} />
+    </Provider>,
+  );
+
+  it('should change all classes when submitted empty', () => {
+    expect(customerSignup.find('input')).toHaveLength(5);
+    customerSignup.find('button').toBeTruthy;
+    expect(customerSignup.find('#firstName').hasClass('fieldInvalid')).toBeTruthy;
+  });
+});

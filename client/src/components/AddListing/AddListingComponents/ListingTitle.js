15e5044488fabe4df99b5fa4ac7bf10250dc5ae2
bobby <bwuphan@gmail.com>
2017-02-21 22:39:06 -0800

    separate add listing title into own component

--- client/src/components/AddListing/AddListingComponents/ListingTitle.js
+++ client/src/components/AddListing/AddListingComponents/ListingTitle.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Form } from 'semantic-ui-react';
 
-export const ListingTitle = ({title, onChange}) => {
+export const ListingTitle = ({ title, onChange }) => {
   return (
-    <Form onSubmit={e => this.onSubmit(e)}>
-      <Form.Field width="8">
-        <label htmlFor="firstName">Title</label>
-        <input
-          placeholder="Insert Title here"
-          name="title"
-          value={title}
-          onChange={e => onChange(e)}
-        />
-      </Form.Field>
-    </Form>
-  )
-}
+    <Form.TextArea
+      rows="1"
+      className="ui center aligned grid"
+      label="Title"
+      placeholder="Insert title of job here"
+      name="title"
+      value={title}
+      onChange={e => onChange(e)}
+    />
+  );
+};
+

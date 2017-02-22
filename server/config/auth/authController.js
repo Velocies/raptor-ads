ce627acd4d12884e5e9412ae90da4aa2dface516
Darkrender <jimmiegisclair@gmail.com>
2017-02-22 13:08:21 -0800

    Implement logIn function in authController

--- server/config/auth/authController.js
+++ server/config/auth/authController.js
@@ -1,10 +1,24 @@
+const db = require('../../../database/schemas.js');
+const bcrypt = require('bcrypt-nodejs');
+
 
 module.exports = {
-  logIn: () => {
-    // TODO: login authorization
+  logIn: (req, res) => {
+    db.User.findOne({ where: { email: req.body.email } })
+    .then((user) => {
+      if (user) {
+        if (bcrypt.compareSync(req.body.password, user.password)) {
+          res.send(user);
+        } else {
+          res.send({ error: 'Incorrect password' });
+        }
+      } else {
+        res.send({ error: 'No account found with that email' });
+      }
+    });
   },
 
-  logOut: () => {
+  logOut: (req, res) => {
     // TODO: logout currently logged in user
   },
 };

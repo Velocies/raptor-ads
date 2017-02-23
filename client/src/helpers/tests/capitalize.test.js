85a239ef46f3f9dfbd7376e2ff4dd1ec2d79296b
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-23 10:27:10 -0800

    Add another test to capitalize

--- client/src/helpers/tests/capitalize.test.js
+++ client/src/helpers/tests/capitalize.test.js
@@ -2,4 +2,5 @@ import { capitalize } from '../capitalize'
 
 test('It capitalizes first letter in string', () => {
   expect(capitalize('cory')).toEqual('Cory');
+  expect(capitalize('Bob')).toEqual('Bob');
 });

2d1bf46e1e821b7bc46e8050698ea4b01dacc6fe
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-23 08:04:52 -0800

    Change AddListing title field from textarea

--- client/src/components/Listing/AddListing/AddListingComponents/ListingTitle.js
+++ client/src/components/Listing/AddListing/AddListingComponents/ListingTitle.js
@@ -3,15 +3,15 @@ import { Form } from 'semantic-ui-react';
 
 export const ListingTitle = ({ title, onChange }) => {
   return (
-    <Form.TextArea
-      rows="1"
-      className="ui center aligned grid"
-      label="Title"
-      placeholder="Insert title of job here"
-      name="title"
-      value={title}
-      onChange={e => onChange(e)}
-    />
+    <Form.Field className="ui center aligned grid">
+      <label>Title</label>
+      <input
+        placeholder="Insert title of job here"
+        name="title"
+        value={title}
+        onChange={e => onChange(e)}
+      />
+    </Form.Field>
   );
 };
 

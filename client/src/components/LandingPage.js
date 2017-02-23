53f2fa98aec9e42694a1ac33f6b26ac47b832c3d
bobby <bwuphan@gmail.com>
2017-02-23 08:52:31 -0800

    add style to landing page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Container, Grid, Item, Link, Statistic } from 'semantic-ui-react';
 
 export const LandingPage = () => {
   return(
-    <h1>HOMEPAGE</h1>
+    <div className="landingContainer">
+      <Grid width={16}>
+        <Grid.Column width={4} />
+        <Grid.Column width={8}>
+          <Item.Image
+            as={Link}
+            to="landing"
+            size="small"
+            src="/client/src/assets/full-raptor.png"
+          />
+          <Item.Content verticalAlign="middle">
+            <Statistic
+              size="large"
+              color="blue"
+              value="Welcome to Raptor Ads"
+            />
+            <div className="landingText">Hello World</div>
+            <Item.Description>
+              <p>Sorry, we cannot find the page you are looking for.</p>
+            </Item.Description>
+          </Item.Content>
+        </Grid.Column>
+      </Grid>
+    </div>
   )
 }

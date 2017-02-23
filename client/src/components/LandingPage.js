c5b2a6ebc107d07ce857beee547ead684e596b39
Bobby Wu Phan <bwuphan@gmail.com>
2017-02-23 10:50:19 -0800

    Revert "add content to landing page"

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,30 +1,7 @@
 import React from 'react';
-import { Container, Grid, Item, Link, Statistic } from 'semantic-ui-react';
 
 export const LandingPage = () => {
   return(
-    <div className="landingContainer">
-      <Grid width={16}>
-        <Grid.Column width={1} />
-        <Grid.Column className="ui right aligned grid" width={4} >
-          <Item.Image
-            as={Link}
-            to="landing"
-            size="small"
-            src="/client/src/assets/full-raptor.png"
-          />
-        </Grid.Column>
-        <Grid.Column width={6}>
-          <Item.Content verticalAlign="middle">
-            <Statistic
-              size="large"
-              color="blue"
-              value="Welcome to Raptor Ads"
-            />
-            <div className="landingText">Where the Professionals come to you</div>
-          </Item.Content>
-        </Grid.Column>
-      </Grid>
-    </div>
+    <h1>HOMEPAGE</h1>
   )
 }

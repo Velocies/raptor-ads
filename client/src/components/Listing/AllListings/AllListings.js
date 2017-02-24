e875478f2434beabc2247305a89c75bf2bf3553d
bobby <bwuphan@gmail.com>
2017-02-24 12:32:09 -0800

    connect alllistings to store

--- client/src/components/Listing/AllListings/AllListings.js
+++ client/src/components/Listing/AllListings/AllListings.js
@@ -3,16 +3,22 @@ import { connect } from 'react-redux';
 import moment from 'moment';
 import { Container, Header, Card, Button, Divider } from 'semantic-ui-react';
 
-export default class AllListings extends Component {
+class AllListings extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
+    console.log('props', this.props);
     return (
       <h2>AllListings</h2>
     );
   }
 }
 
-// const mapStateToProps = (state) => {
+const mapStateToProps = (state) => {
+  const { allListings } = state.listing;
+  return { allListings };
+}
 
-// }
-
-// export default connect
+export default connect(mapStateToProps)(AllListings);

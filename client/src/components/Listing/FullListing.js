234e4f2d978763e4c778988d858f68347c5e6401
Darkrender <jimmiegisclair@gmail.com>
2017-02-25 12:28:38 -0800

    Add basic header to FullListing page

--- client/src/components/Listing/FullListing.js
+++ client/src/components/Listing/FullListing.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { Container, Grid, Icon, Header, Divider } from 'semantic-ui-react';
 
 class FullListing extends Component {
   constructor(props) {
@@ -8,20 +9,23 @@ class FullListing extends Component {
 
   render() {
     const { title, body, images, createdAt } = this.props;
+    console.log('FullListing props: ', this.props);
     return (
-      <Container text>
-        <h1>{title}</h1>
-        <p>{body}</p>
+      <Container textAlign="center">
+        <Header as="h1" className="center">-- {title || 'Current Listing'} --</Header>
+        <Divider />
+
       </Container>
     );
   }
 }
 
-FullListing.propTypes = {
-  title: React.PropTypes.string.isRequired,
-  body: React.PropTypes.string.isRequired,
-  images: React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
-  createdAt: React.PropTypes.string.isRequired,
+const mapStateToProps = (state) => {
+  const { listingForm } = state.listing;
+  const { id } = state.auth.loggedInUser;
+  return {
+    id,
+    listingForm,
+  };
 };
-
-export default FullListing;
+export default connect(mapStateToProps)(FullListing);

dafeffbfbf131fc580c28cfa6df3b6417fc3d294
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-25 12:57:20 -0800

    Delete duplicate helpers directory

--- client/src/components/Dashboard/CustomerDashboard.js
+++ client/src/components/Dashboard/CustomerDashboard.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import { Container, Header, Card, Button, Divider, Loader } from 'semantic-ui-react';
 import Listing from '../shared/Listing';
 import { fetchUserListings, removeListing } from '../../actions';
+import { capitalize } from '../../helpers/capitalize';
 
 class CustomerDashboard extends Component {
   constructor(props) {
@@ -21,45 +22,42 @@ class CustomerDashboard extends Component {
 
   render() {
     const { first_name, userListings, id, isFetching } = this.props;
-    console.log('props', this.props)
-    if (isFetching) {
-      return <Loader active inline='centered' />;
-    } else {
-      return (
-        <Container textAlign="center">
-          <Header as="h1" className="center">Dashboard</Header>
-          <h3>Recent Listings</h3>
-          <Divider />
-          <Card.Group itemsPerRow={4} stackable>
-            {userListings && userListings.map(listing =>
-              <Listing
-                key={listing.id}
-                id={listing.id}
-                title={listing.title}
-                createdAt={this.convertTime(listing.createdAt)}
-                body={listing.body}
-                type={listing.type}
-                onClick={this.onClick}
-                handleDelete={this.handleDelete}
-              />
-            )}
-          </Card.Group>
-        </Container>
-      );
-    }
+    if (isFetching) { return <Loader active inline="centered" />; }
+    return (
+      <Container textAlign="center">
+        <Header as="h1" className="center">Dashboard</Header>
+        <h3>Recent Listings</h3>
+        <Divider />
+        <Card.Group itemsPerRow={4} stackable>
+          {userListings && userListings.map(listing =>
+            <Listing
+              key={listing.id}
+              id={listing.id}
+              title={listing.title}
+              createdAt={this.convertTime(listing.createdAt)}
+              body={listing.body}
+              type={listing.type}
+              onClick={this.onClick}
+              handleDelete={this.handleDelete}
+            />,
+          )}
+        </Card.Group>
+      </Container>
+    );
   }
 }
 
 CustomerDashboard.propTypes = {
   first_name: React.PropTypes.string.isRequired,
-  userListings: React.PropTypes.array.isRequired,
+  userListings:
+    React.PropTypes.arrayOf(React.PropTypes.object).isRequired,
   id: React.PropTypes.number.isRequired,
+  isFetching: React.PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = (state) => {
   const { first_name, id } = state.auth.loggedInUser;
   const { userListings, isFetching } = state.listing;
-  console.log('id', id);
 
   return { first_name, id, userListings, isFetching };
 };

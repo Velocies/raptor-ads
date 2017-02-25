7f4027ff76adc96752d66c519938eeb2300cd47b
bobby <bwuphan@gmail.com>
2017-02-24 16:33:11 -0800

    working on fetching all listings

--- client/src/actions/allListingActions.js
+++ client/src/actions/allListingActions.js
@@ -1,5 +1,20 @@
-import { GET_ALL_LISTINGS } from '../constants';
+import { GET_ALL_LISTINGS, GET_ALL_LISTINGS_SUCCESS } from '../constants';
+import { fetchAllListings } from './api';
+
+const getAllListingsSuccess = payload =>
+  ({
+
+  })
 
 export const getAllListings = () => {
-  console.log('hello from actions');
-}
+  (dispatch) => {
+    fetchAllListings()
+    .then((res) => {
+      res.json()
+      .then((data) => {
+        dispatch(getAllListingsSuccess(data));
+      });
+    });
+
+  };
+};

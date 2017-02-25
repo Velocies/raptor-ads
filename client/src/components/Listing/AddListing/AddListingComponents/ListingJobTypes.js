e3f976fc35abae592d5bdf25d34b99880905c710
bobby <bwuphan@gmail.com>
2017-02-25 00:52:28 -0800

    fix styling errors for add listing components

--- client/src/components/Listing/AddListing/AddListingComponents/ListingJobTypes.js
+++ client/src/components/Listing/AddListing/AddListingComponents/ListingJobTypes.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Form, Dropdown } from 'semantic-ui-react';
 import { jobCategories } from '../../jobCategories';
 
-const ListingJobTypes = ({ onChange, type }) => {
-  return (
-    <Form.Field className="ui center aligned grid">
-      <label htmlFor="category">Job Category</label>
-      <Dropdown
-        fluid
-        selection
-        options={jobCategories}
-        placeholder="Select type of job here"
-        onChange={(e, data) => onChange(e, data)}
-        label="Profession"
-      />
-    </Form.Field>
-  );
+const ListingJobTypes = ({ onChange }) =>
+  <Form.Field className="ui center aligned grid">
+    <label htmlFor="category">Job Category</label>
+    <Dropdown
+      fluid
+      selection
+      options={jobCategories}
+      placeholder="Select type of job here"
+      onChange={(e, data) => onChange(e, data)}
+      label="Profession"
+    />
+  </Form.Field>;
+
+ListingJobTypes.propTypes = {
+  onChange: React.PropTypes.func.isRequired,
 };
 
 export default ListingJobTypes;

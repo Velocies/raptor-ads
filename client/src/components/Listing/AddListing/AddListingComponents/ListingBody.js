9474dda282a1db2e5e2e51750c6ec4abe5396638
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-27 12:24:19 -0800

    Add form class to textarea

--- client/src/components/Listing/AddListing/AddListingComponents/ListingBody.js
+++ client/src/components/Listing/AddListing/AddListingComponents/ListingBody.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { Form } from 'semantic-ui-react';
 
-const ListingBody = ({ body, onChange }) =>
+const ListingBody = ({ body, onChange, getFormClass }) =>
   <Form.TextArea
     rows="5"
-    className='ui center aligned grid'
     label="Job Description"
     placeholder="Tell us about the job..."
     name="body"
     value={body}
     onChange={e => onChange(e)}
+    className={getFormClass('body')}
   />;
 
 
 ListingBody.propTypes = {
   body: React.PropTypes.string.isRequired,
   onChange: React.PropTypes.func.isRequired,
+  getFormClass: React.PropTypes.func.isRequired,
 };
 
 export default ListingBody;

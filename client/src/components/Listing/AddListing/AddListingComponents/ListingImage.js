824d5d2d4c914e5a176c92e24b85de305ec0bc0b
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-27 12:32:50 -0800

    Add border to AddListing Image field

--- client/src/components/Listing/AddListing/AddListingComponents/ListingImage.js
+++ client/src/components/Listing/AddListing/AddListingComponents/ListingImage.js
@@ -2,21 +2,21 @@ import React from 'react';
 import { Form, Input, Icon } from 'semantic-ui-react';
 
 const ListingImage = ({ onClick, onChange, image, getFormClass }) =>
-  <Form.Field>
+  <Form.Field className={getFormClass('image')}>
     <Input
       icon={<Icon onClick={() => onClick(image)} name="plus" inverted circular link />}
       placeholder="Add image URL here"
       value={image}
       name="image"
       onChange={e => onChange(e)}
-      className={getFormClass('image')}
     />
-  </Form.Field>
+  </Form.Field>;
 
 ListingImage.propTypes = {
   onClick: React.PropTypes.func.isRequired,
   onChange: React.PropTypes.func.isRequired,
   image: React.PropTypes.string.isRequired,
+  getFormClass: React.PropTypes.func.isRequired,
 };
 
 

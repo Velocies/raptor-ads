590ec52c35e63b9c9a6b2bbf862ec5e1462e4b6c
tyler <tyler@jackoldmacair.local>
2017-02-27 12:15:31 -0800

    add user profile page landing

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>PROFILE COMPONENT HERE</h1>
+      </div>
+    );
+  }
+}
+
+export default Profile;

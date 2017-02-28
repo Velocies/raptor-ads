208bdcfce8dbae2c54aedb16b61a2fd1a1461e5d
Darkrender <jimmiegisclair@gmail.com>
2017-02-28 11:09:13 -0800

    Add related listings at bottom of FullListing page

--- client/src/components/Listing/FullListing.js
+++ client/src/components/Listing/FullListing.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import Carousel from 'nuka-carousel';
+import moment from 'moment';
 import { Container, Grid, Image, Header, Divider, Message, List, Loader } from 'semantic-ui-react';
-import { getCurrentListing } from '../../actions/fullListingActions';
 import GoogleMapContainer from './AllListings/AllListingsComponents/GoogleMap/GoogleMapContainer';
+import { getCurrentListing } from '../../actions/fullListingActions';
+import Listing from '../shared/Listing';
 
 class FullListing extends Component {
   constructor(props) {
@@ -12,12 +15,26 @@ class FullListing extends Component {
   }
 
   componentDidMount() {
+    console.log('Listing ID: ', this.listingId);
     this.props.dispatch(getCurrentListing(this.listingId));
   }
 
+  convertTime(time) {
+    return moment(time).fromNow();
+  }
+
+  cutBody(body) {
+    let newBody;
+    if (body.length > 20) {
+      newBody = `${body.slice(0, 20)}...`;
+      return newBody;
+    }
+    return body;
+  }
+
   render() {
-    const { isFetching, currentListing } = this.props;
-    console.log('FullListing props: ', this.props);
+    const { isFetching, currentListing, userListings } = this.props;
+    console.log('Listing ID: ', this.listingId);
 
     if (isFetching) {
       return <Loader active inline="centered" />;
@@ -26,7 +43,7 @@ class FullListing extends Component {
     return (
       <Container textAlign="center">
         <Header as="h1" className="center">-- {currentListing.title || 'Current Listing'} -- <br /></Header>
-        <Header as="h4" className="center" color="grey">{currentListing.createdAt || 'Time Created'}</Header>
+        <Header as="h4" className="center" color="grey">{`Created ${this.convertTime(currentListing.createdAt)}` || 'Time Created'}</Header>
 
         <Divider />
 
@@ -41,9 +58,7 @@ class FullListing extends Component {
                 <Divider hidden section />
                 <Carousel>
                   {
-                    currentListing.pictures.map((p, index) => {
-                      return <Image key={index} src={p.img_path} />;
-                    })
+                    currentListing.pictures.map(p => <Image key={p.id} src={p.img_path} />)
                   }
                 </Carousel>
               </Grid.Row>
@@ -87,13 +102,58 @@ class FullListing extends Component {
             <Header as="h3" className="center">Related Listings</Header>
             <Grid.Row columns={3}>
               <Grid.Column>
-                <Image src="http://semantic-ui.com/images/wireframe/media-paragraph.png" />
+                {
+                  (() => {
+                    if (userListings[0]) {
+                      return (<Listing
+                        key={userListings[0].id}
+                        listingId={userListings[0].id}
+                        title={userListings[0].title}
+                        createdAt={this.convertTime(userListings[0].createdAt)}
+                        body={userListings[0].body}
+                        type={userListings[0].type}
+                        cutBody={this.cutBody}
+                      />);
+                    }
+                    return <Image src="http://semantic-ui.com/images/wireframe/media-paragraph.png" />;
+                  })()
+                }
               </Grid.Column>
               <Grid.Column>
-                <Image src="http://semantic-ui.com/images/wireframe/media-paragraph.png" />
+                {
+                  (() => {
+                    if (userListings[1]) {
+                      return (<Listing
+                        key={userListings[1].id}
+                        listingId={userListings[1].id}
+                        title={userListings[1].title}
+                        createdAt={this.convertTime(userListings[1].createdAt)}
+                        body={userListings[1].body}
+                        type={userListings[1].type}
+                        cutBody={this.cutBody}
+                      />);
+                    }
+                    return <Image src="http://semantic-ui.com/images/wireframe/media-paragraph.png" />;
+                  })()
+                }
               </Grid.Column>
               <Grid.Column>
-                <Image src="http://semantic-ui.com/images/wireframe/media-paragraph.png" />
+                {
+                  (() => {
+                    if (userListings[2]) {
+                      return (<Listing
+                        key={userListings[2].id}
+                        listingId={userListings[2].id}
+                        title={userListings[2].title}
+                        createdAt={this.convertTime(userListings[2].createdAt)}
+                        body={userListings[2].body}
+                        type={userListings[2].type}
+                        cutBody={this.cutBody}
+                      />);
+                    }
+                    return <Image src="http://semantic-ui.com/images/wireframe/media-paragraph.png" />;
+                  })()
+                }
               </Grid.Column>
             </Grid.Row>
           </Grid>
@@ -105,11 +165,12 @@ class FullListing extends Component {
 }
 
 const mapStateToProps = (state) => {
-  const { currentListing, isFetching } = state.listing;
+  const { currentListing, userListings, isFetching } = state.listing;
   const { id } = state.auth.loggedInUser;
   return {
     id,
     currentListing,
+    userListings,
     isFetching,
   };
 };

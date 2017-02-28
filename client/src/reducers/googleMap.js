58645572af34bb7f65bf54f66f4a442f1c87ebd7
bobby <bwuphan@gmail.com>
2017-02-28 12:26:51 -0800

    add recenter for google map reducer

--- client/src/reducers/googleMap.js
+++ client/src/reducers/googleMap.js
@@ -1,13 +1,15 @@
-import { ADD_MAP_MARKER } from '../constants';
+import { ADD_MAP_MARKER, CHANGE_CENTER } from '../constants';
 
 export const initialState = {
-  center: { lat: 33, lng: 131.044922 },
+  center: { lat: 20, lng: -30 },
   markers: [],
-}
+};
 
 export const googleMap = (state = initialState, action) => {
   console.log('action here', action);
   switch (action.type) {
+    case CHANGE_CENTER:
+      return ({ ...state, center: action.location });
     case ADD_MAP_MARKER:
       return ({ ...state, markers: [...state.markers, action.location ] });
     default:

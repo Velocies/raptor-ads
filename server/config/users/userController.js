e362317cde5687531b8c689482dfc6527def73ff
Cory Wolnewitz <wolnewitz@gmail.com>
2017-02-28 14:54:29 -0800

    Associate business with user

--- server/config/users/userController.js
+++ server/config/users/userController.js
@@ -8,22 +8,21 @@ module.exports = {
       where: {
         id: req.params.id,
       },
-      include: { [models.Business] },
-    })
-    .then((user) => {
-      if (user) {
-        res.json(user);
-      } else {
-        res.status(400).send('User not found!');
-      }
-    });
+    }, { include: [models.Business] })
+      .then((user) => {
+        if (user) {
+          res.json(user);
+        } else {
+          res.status(400).send('User not found!');
+        }
+      });
   },
 
   getAll: (req, res) => {
-    models.User.findAll({})
-    .then((users) => {
-      res.send(users);
-    });
+    models.User.findAll({ include: [models.Business] })
+      .then((users) => {
+        res.send(users);
+      });
   },
 
   createOne: (req, res) => {
@@ -32,37 +31,38 @@ module.exports = {
         email: req.body.email,
       },
     })
-    .then((user) => {
-      if (!user) {
-        const salt = bcrypt.genSaltSync(10);
-        const passwordToSave = bcrypt.hashSync(req.body.password, salt);
+      .then((user) => {
+        console.log('BODY', req.body);
+        if (!user) {
+          const salt = bcrypt.genSaltSync(10);
+          const passwordToSave = bcrypt.hashSync(req.body.password, salt);
 
-        models.User.create({
-          firstName: req.body.firstName,
-          lastName: req.body.lastName,
-          password: passwordToSave,
-          email: req.body.email,
-          address: req.body.address,
-          city: req.body.city,
-          state: req.body.state,
-          zip: req.body.zip,
-          country: req.body.country,
-          role: req.body.role,
+          models.User.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            password: passwordToSave,
+            email: req.body.email,
+            address: req.body.address,
+            city: req.body.city,
+            state: req.body.state,
+            zip: req.body.zip,
+            country: req.body.country,
+            role: req.body.role,
+            business: req.body.business,
+          }, { include: [models.Business] })
+            .then((createdUser) => {
+              const token = jwt.sign({
+                email: createdUser.email,
+                firstName: createdUser.firstName,
+                id: createdUser.id,
+              }, 'bobbyisbadatstarcraft', { expiresIn: '1h' });
 
-        })
-        .then((createdUser) => {
-          const token = jwt.sign({
-            email: createdUser.email,
-            firstName: createdUser.firstName,
-            id: createdUser.id,
-          }, 'bobbyisbadatstarcraft', { expiresIn: '1h' });
-
-          res.json({ token, user: createdUser });
-        });
-      } else {
-        res.send({ error: 'User already exists!' });
-      }
-    });
+              res.json({ token, user: createdUser });
+            });
+        } else {
+          res.send({ error: 'User already exists!' });
+        }
+      });
   },
 
   patchOne: (req, res) => {
@@ -70,10 +70,10 @@ module.exports = {
       where: {
         id: req.params.id,
       },
-    })
-    .then((status) => {
-      res.send(status);
-    });
+    }, { include: [models.Business] })
+      .then((status) => {
+        res.send(status);
+      });
   },
 
   deleteOne: (req, res) => {
@@ -82,12 +82,12 @@ module.exports = {
         id: req.params.id,
       },
     })
-    .then((status) => {
-      if (status === 1) {
-        res.send('User deleted!');
-      } else {
-        res.send('User did not exist');
-      }
-    });
+      .then((status) => {
+        if (status === 1) {
+          res.send('User deleted!');
+        } else {
+          res.send('User did not exist');
+        }
+      });
   },
 };

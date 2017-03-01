ad8ddd75d71fd0d10c7691935e8b788c418bfe95
Cory Wolnewitz <wolnewitz@gmail.com>
2017-03-01 14:59:05 -0800

    Add .eslintrc

--- server/config/messages/messagesController.js
+++ server/config/messages/messagesController.js
@@ -1,5 +1,11 @@
 const models = require('../../../database/schemas.js');
 
 module.exports = {
-  
+  getAllForUser: (req, res) => {
+    res.send(200);
+  },
+
+  createMessage: (req, res) => {
+    res.send(200);
+  }
 };

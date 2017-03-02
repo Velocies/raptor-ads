344991c98b0b507903af03c1b95b0a1a30875deb
Cory Wolnewitz <wolnewitz@gmail.com>
2017-03-02 10:36:28 -0800

    Add userId to UserDetails and connect it

--- client/src/components/UserDetails/UserDetails.js
+++ client/src/components/UserDetails/UserDetails.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class UserDetails extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return <h2>UserDetails</h2>;
   }
 }
 
-export default connect(state=>state)(UserDetails);
+const mapStateToProps = (state) => {
+  const { pathname } = state.routing.locationBeforeTransitions;
+  const userId = pathname.split('/')[2];
+
+  return ({ userId });
+};
+
+
+export default connect(mapStateToProps)(UserDetails);

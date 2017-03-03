a6b7b7797726081cf87a8e5330742b7394ab0435
Cory Wolnewitz <wolnewitz@gmail.com>
2017-03-03 14:22:52 -0800

    Use production db

--- database/database.js
+++ database/database.js
@@ -1,8 +1,18 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('postgres', 'postgres', ' ', {
-  dialect: 'postgres',
-  port: 5432,
-});
+if (process.env.DATABASE_URL) {
+  let db = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    port:     match[4],
+    host:     match[3],
+    logging:  true
+  });
+} else {
+  let db = new Sequelize('postgres', 'postgres', ' ', {
+    dialect: 'postgres',
+    port: 5432,
+  });
+}
 
 module.exports = { database: db, Sequelize };

2ec4b4fe3c024c3e62dba011c250a0e3b0f3c689
Cory Wolnewitz <wolnewitz@gmail.com>
2017-03-03 14:02:56 -0800

    Logger only in development take 3

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,16 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
-import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import { routerMiddleware } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 import rootReducer from './reducers';
-
 
 const configureStore = () => {
   const router = routerMiddleware(browserHistory);
   const middlewares = [thunk, router];
 
   if (!process.env.NODE_ENV) {
+    const createLogger = require('redux-logger');
     const logger = createLogger();
     middlewares.push(logger);
   }

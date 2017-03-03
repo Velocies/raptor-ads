8a8708c6f8662e2ed96bfe25eea53f4472f54e08
Cory Wolnewitz <wolnewitz@gmail.com>
2017-03-03 13:58:02 -0800

    Only use logger in development

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -5,14 +5,17 @@ import { routerMiddleware } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 import rootReducer from './reducers';
 
-const middleware = routerMiddleware(browserHistory);
 
 const configureStore = () => {
-  if (process.env.NODE_ENV === 'production') {
-    return createStore(rootReducer, applyMiddleware(thunk, middleware));
+  const router = routerMiddleware(browserHistory);
+  const middlewares = [thunk, router];
+
+  if (!process.env.NODE_ENV) {
+    const logger = createLogger();
+    middlewares.push(logger);
   }
-  const logger = createLogger();
-  return createStore(rootReducer, applyMiddleware(logger, thunk, middleware));
+
+  return createStore(rootReducer, applyMiddleware(...middlewares));
 };
 
 export default configureStore;

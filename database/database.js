18d75599997f8609ce70a951ab5865b700e46af7
Cory Wolnewitz <wolnewitz@gmail.com>
2017-03-03 14:24:56 -0800

    Use heroku db in production

--- database/database.js
+++ database/database.js
@@ -1,15 +1,17 @@
 const Sequelize = require('sequelize');
 
+let db;
+
 if (process.env.DATABASE_URL) {
-  let db = new Sequelize(process.env.DATABASE_URL, {
-    dialect:  'postgres',
-    protocol: 'postgres',
-    port:     match[4],
-    host:     match[3],
-    logging:  true
+  db = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    protocol:'postgres',
+    port: match[4],
+    host: match[3],
+    logging: true
   });
 } else {
-  let db = new Sequelize('postgres', 'postgres', ' ', {
+  db = new Sequelize('postgres', 'postgres', ' ', {
     dialect: 'postgres',
     port: 5432,
   });

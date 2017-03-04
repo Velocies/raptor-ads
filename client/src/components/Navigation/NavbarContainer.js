14f6e3cdf94f00dcf4ee7c6e0142d17bf1408c1f
Cory Wolnewitz <wolnewitz@gmail.com>
2017-03-03 19:42:28 -0800

    Fix routingBeforeTransitions being null to start

--- client/src/components/Navigation/NavbarContainer.js
+++ client/src/components/Navigation/NavbarContainer.js
@@ -2,8 +2,11 @@ import { connect } from 'react-redux';
 import Navbar from './Navbar';
 import { logout } from '../../actions';
 
-const mapStateToProps = (state) => {
-  const location = state.routing.locationBeforeTransitions.pathname;
+const mapStateToProps = (state, ownProps) => {
+  let location = '';
+  if (state.routing.locationBeforeTransitions !== null) {
+    location = state.routing.locationBeforeTransitions.pathname;
+  }
   const { id  } = state.auth.loggedInUser;
 
   return { location, id };

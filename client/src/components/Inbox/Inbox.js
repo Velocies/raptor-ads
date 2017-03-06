2c4a4a73ddf6ab84d873ccb76219499b58b8ec05
Darkrender <jimmiegisclair@gmail.com>
2017-03-06 11:24:56 -0800

    Create and set up basic inbox component

---
+++ client/src/components/Inbox/Inbox.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import moment from 'moment';
+import { Container, Grid, Image, Header, Divider, Message, List, Loader, Button, Modal, Form, Card } from 'semantic-ui-react';
+
+class Inbox extends Component {
+
+  render() {
+    return (
+      <Container textAlign="center">
+        <Header as="h1" className="center">-- Inbox -- <br /></Header>
+      </Container>
+    );
+  }
+}

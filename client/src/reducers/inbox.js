b92cd9fc60b1b4bc27599b0141818ec24b4ffd24
Darkrender <jimmiegisclair@gmail.com>
2017-03-06 16:50:06 -0800

    Implement inbox GET_RECEIVED_MESSAGES_SUCCESS case

--- client/src/reducers/inbox.js
+++ client/src/reducers/inbox.js
@@ -1,4 +1,4 @@
-import { FETCHING_MESSAGES } from '../constants';
+import { FETCHING_MESSAGES, GET_RECEIVED_MESSAGES_SUCCESS } from '../constants';
 
 
 export const initialState = {
@@ -11,6 +11,8 @@ export const inbox = (state = initialState, action) => {
   switch (action.type) {
     case FETCHING_MESSAGES:
       return ({ ...state, isFetching: true });
+    case GET_RECEIVED_MESSAGES_SUCCESS:
+      return ({ ...state, allMessages: action.payload, isFetching: false });
     default:
       return state;
   }

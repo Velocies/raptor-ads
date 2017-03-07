9e6aebb70f85c559abcc9eefb67a2cb64cc10c6c
bobby <bwuphan@gmail.com>
2017-03-06 16:38:31 -0800

    wip listing info card

--- client/src/components/Listing/AllListings/AllListingsComponents/ListingInfoCard.js
+++ client/src/components/Listing/AllListings/AllListingsComponents/ListingInfoCard.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import { Card, Header, Image, Icon } from 'semantic-ui-react';
+import convertDistance from '../../../helpers/convertDistance';
 
 
-const description =
-  'Amy is a violinist with 2 years experience in the wedding industry. She enjoys the outdoors and currently resides in upstate New York.'
-
-
-const ListingInfoCard = () => (
-  <Card.Group className="ui center aligned grid" style={{marginTop: '2px'}}>
-    <Card fluid className="ui center aligned grid" style={{marginTop: '2px'}}>
-    <Card.Content header='About Amy' />
-    <Card.Content>
-      <Image floated='right' size='mini' src='http://semantic-ui.com/images/avatar/large/steve.jpg' />
-    </Card.Content>
-    <Card.Content description={description} />
-    <Card.Content extra>
-      <Icon name='user' />
-      4 Friends
-    </Card.Content>
-  </Card>
-  </Card.Group>
-)
+const ListingInfoCard = ({ card }) => {
+  if (card) {
+    let picturePath;
+    if (card.pictures[0]) {
+      picturePath = card.pictures[0].img_path
+    } else {
+      picturePath = 'http://www.satcomresources.com/c.788689/vsat/img/no_image_available.jpeg'
+    }
+    return (
+      <Card.Group className="ui center aligned grid" style={{marginTop: '2px'}}>
+        <Card fluid style={{marginTop: '2px'}}>
+        <Card.Content header={card.title} />
+        <Card.Content style={{height: '160px'}}>
+          <Image floated='left' size='small' src={picturePath} /> <Card.Content description={card.body} />
+        </Card.Content>
+        <Card.Content style={{height: '40px'}}>
+          {card.distanceFromCenter && (<Card.Content description={`${convertDistance(card.distanceFromCenter)} miles away`} />)}
+        </Card.Content>
+      </Card>
+      </Card.Group>
+    )
+  } else {
+    return (<div></div>)
+  }
+};
 
 export default ListingInfoCard;

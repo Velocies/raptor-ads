f6b8f4dd845d616fc79edf89a844c0584afd4897
Darkrender <jimmiegisclair@gmail.com>
2017-03-06 17:03:38 -0800

    Implement initial props setup for Inbox component

--- client/src/components/Profile/Inbox/Inbox.js
+++ client/src/components/Profile/Inbox/Inbox.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import moment from 'moment';
-import { Container, Grid, Image, Header, Divider, Message, List, Loader, Button, Modal, Form, Card } from 'semantic-ui-react';
+import { Container, Header, Loader } from 'semantic-ui-react';
+import { getAllReceivedMessages } from '../../../actions/inboxActions';
 
 class Inbox extends Component {
 
+  componentDidMount() {
+    this.props.dispatch(getAllReceivedMessages(this.props.userId));
+  }
+
   render() {
+    const { isFetching } = this.props;
+
+    if (isFetching) {
+      return <Loader active inline="centered" />;
+    }
+
     return (
       <Container textAlign="center">
         <Header as="h1" className="center">-- Inbox -- <br /></Header>
@@ -15,7 +26,21 @@ class Inbox extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return ({});
+  const { isFetching, allMessages } = state.inbox;
+  const { id } = state.auth.loggedInUser;
+  const userId = id;
+
+  return {
+    userId,
+    allMessages,
+    isFetching,
+  };
+};
+
+Inbox.propTypes = {
+  userId: React.PropTypes.number.isRequired,
+  isFetching: React.PropTypes.bool.isRequired,
+  dispatch: React.PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps)(Inbox);
